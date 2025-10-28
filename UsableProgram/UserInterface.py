8888ed4d60b744d4c051a4d447d63c402a80091b
hgf700 <pawellapinski33@gmail.com>
2025-10-28 19:51:51 +0100

    feat: update ui

--- UsableProgram/UserInterface.py
+++ UsableProgram/UserInterface.py
@@ -2,17 +2,26 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
+root.overrideredirect(True)
 
-mainframe = ttk.Frame(root, padding=(1,1,1,1))
+# root.geometry("300x200+500+300")
+
+mainframe = ttk.Frame(root)
 mainframe.grid(column=0, row=0)
 
+def close_window():
+    root.destroy()
+
+close_button = ttk.Button(mainframe, text="X", command=close_window)
+close_button.grid(column=2, row=2, sticky=W) 
+
 scale = StringVar()
-ttk.Label(mainframe, text="S").grid(column=2, row=0, sticky=E)
+ttk.Label(mainframe, text="S").grid(column=2, row=0, sticky=W)
 scale_entry = ttk.Entry(mainframe, textvariable=scale, state="readonly", width=10)
 scale_entry.grid(column=1, row=0, sticky=(W))
 
 meters = StringVar()
-ttk.Label(mainframe, text="M").grid(column=2, row=1, sticky=E)
+ttk.Label(mainframe, text="M").grid(column=2, row=1, sticky=W)
 meters_entry = ttk.Entry(mainframe, textvariable=meters, state="readonly", width=10)
 meters_entry.grid(column=1, row=1, sticky=(W))
 
@@ -23,18 +32,19 @@ def asd():
     except ValueError:
         pass
 
+manual_setting_button = ttk.Button(mainframe, text="Scale", command=asd)
+
 def mode_changed(*args):
     if mode.get() == "manual":
-        manual_setting_button = ttk.Button(mainframe, text="Scale", command=asd)
-        manual_setting_button.grid(column=0, row=2)
+        manual_setting_button.grid(column=1, row=3) 
     else:
-        manual_setting_button.grid_remove()
+        manual_setting_button.grid_remove()  
 
 mode = StringVar(value="auto")
 auto_button = ttk.Radiobutton(mainframe, text="A", variable=mode, value="auto", command=mode_changed)
 manual_button = ttk.Radiobutton(mainframe, text="M", variable=mode, value="manual",command=mode_changed)
 auto_button.grid(column=1, row=2, sticky=E)
-manual_button.grid(column=2, row=2, sticky=W)
+manual_button.grid(column=1, row=2, sticky=W)
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
@@ -42,4 +52,5 @@ mainframe.columnconfigure(2, weight=1)
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
+manual_setting_button.grid_remove()
 root.mainloop()
