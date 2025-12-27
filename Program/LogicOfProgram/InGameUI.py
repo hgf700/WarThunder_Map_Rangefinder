b8e4bbfa0ca0990f6081e6f72d9a92e813e5fb8f
hgf700 <148117806+hgf700@users.noreply.github.com>
2025-12-27 02:36:43 +0100

    refactor: small

--- Program/LogicOfProgram/InGameUI.py
+++ Program/LogicOfProgram/InGameUI.py
@@ -61,17 +61,15 @@ def InGameUI():
 
     def open_scale():
         value = ManualScale(root)  
-        if value is not None:
-            scale.set(value)
-            logger.debug(f"manually seted scale {value}")
-            print(f"ingameui: {value}")
+        scale.set(value)
+        logger.debug(f"manually seted scale {value}")
+        print(f"ingameui: {value}")
 
     def auto_set_scale():
         value = OCR_A_andTresholdingPhoto()  
-        if value is not None:
-            scale.set(value)
-            logger.debug(f"auto seted scale {value}")
-            print(f"ingameui: {value}")
+        scale.set(value)
+        logger.debug(f"auto seted scale {value}")
+        print(f"ingameui: {value}")
 
     def start_move(event):
         root.x = event.x
